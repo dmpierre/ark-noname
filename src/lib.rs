d85ebf493b89c403d8c9f8e5e72d9bcfc0e3d51e
dmpierre <pdaixmoreux@gmail.com>
2024-06-26 18:12:35 +0200

    chore: add test with an array of public outputs

--- src/lib.rs
+++ src/lib.rs
@@ -10,17 +10,22 @@ use noname::backends::{
 use noname::witness::CompiledCircuit;
 use num_bigint::BigUint;
 
-struct NoNameCircuit<BF: BackendField> {
+pub mod circuits;
+pub mod utils;
+
+//#[derive(Debug, Clone)]
+pub struct NoNameCircuit<BF: BackendField> {
     compiled_circuit: CompiledCircuit<R1CS<BF>>,
     witness: GeneratedWitness<BF>,
 }
+
 impl<F: PrimeField, BF: BackendField> ConstraintSynthesizer<F> for NoNameCircuit<BF> {
     fn generate_constraints(self, cs: ConstraintSystemRef<F>) -> Result<(), SynthesisError> {
         let public_io_length = self.compiled_circuit.circuit.backend.public_inputs.len()
             + self.compiled_circuit.circuit.backend.public_outputs.len();
 
         // arkworks assigns by default the 1 constant
-        // assumes witness is: [1, public_inputs, public_outputs, private_inputs, aux]
+        // assumes witness is: [1, public_outputs, public_inputs, private_inputs, aux]
         let witness_size = self.witness.witness.len();
         for idx in 1..witness_size {
             let value: BigUint = Into::into(self.witness.witness[idx]);
@@ -71,43 +76,13 @@ impl<F: PrimeField, BF: BackendField> ConstraintSynthesizer<F> for NoNameCircuit
 
 #[cfg(test)]
 mod tests {
+
     use super::*;
     use ark_bn254::Fr;
     use ark_relations::r1cs::ConstraintSystem;
-    use noname::{
-        backends::r1cs::R1csBn254Field,
-        circuit_writer::CircuitWriter,
-        compiler::{typecheck_next_file, Sources},
-        inputs::parse_inputs,
-        type_checker::TypeChecker,
-    };
-    const SIMPLE_ADDITION: &str = "fn main(pub public_input: Field, private_input: Field) {
-    let xx = private_input + public_input;
-    let yy = private_input * public_input;
-    assert_eq(xx, yy);
-}
-";
-    pub fn compile_source_code<BF: BackendField>(
-        code: &str,
-    ) -> Result<CompiledCircuit<R1CS<BF>>, noname::error::Error> {
-        let mut sources = Sources::new();
-
-        // parse the transitive dependency
-        let mut tast = TypeChecker::<R1CS<BF>>::new();
-        let mut node_id = 0;
-        node_id = typecheck_next_file(
-            &mut tast,
-            None,
-            &mut sources,
-            "main.no".to_string(),
-            code.to_string(),
-            node_id,
-        )
-        .unwrap();
-        let r1cs = R1CS::<BF>::new();
-        // compile
-        CircuitWriter::generate_circuit(tast, r1cs)
-    }
+    use circuits::{SIMPLE_ADDITION, WITH_PUBLIC_OUTPUT_ARRAY};
+    use noname::{backends::r1cs::R1csBn254Field, inputs::parse_inputs};
+    use utils::compile_source_code;
 
     #[test]
     fn cs_is_satisfied() {
@@ -121,6 +96,28 @@ mod tests {
             .generate_witness(json_public, json_private)
             .unwrap();
 
+        let noname_circuit = NoNameCircuit {
+            compiled_circuit,
+            witness: generated_witness,
+        };
+
+        let cs = ConstraintSystem::<Fr>::new_ref();
+        noname_circuit.generate_constraints(cs.clone()).unwrap();
+        assert!(cs.is_satisfied().unwrap());
+    }
+
+    #[test]
+    fn cs_is_satisfied_array() {
+        let compiled_circuit =
+            compile_source_code::<R1csBn254Field>(WITH_PUBLIC_OUTPUT_ARRAY).unwrap();
+        let inputs_public = r#"{"public_input": ["2", "5"]}"#;
+        let inputs_private = r#"{"private_input": ["8", "2"]}"#;
+
+        let json_public = parse_inputs(inputs_public).unwrap();
+        let json_private = parse_inputs(inputs_private).unwrap();
+        let generated_witness = compiled_circuit
+            .generate_witness(json_public, json_private)
+            .unwrap();
         let noname_circuit = NoNameCircuit {
             compiled_circuit,
             witness: generated_witness,
